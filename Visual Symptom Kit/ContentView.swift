9b64a44a2139e18ff830fa4ce47a24aabc819786
Afeez <harfyzz@gmail.com>
2024-10-06 13:09:07 +0100

    Initial Commit

---
+++ Visual Symptom Kit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Visual Symptom Kit
+//
+//  Created by Afeez Yunus on 06/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Visual Symptom Kit/Visual_Symptom_KitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Visual_Symptom_KitApp.swift
+//  Visual Symptom Kit
+//
+//  Created by Afeez Yunus on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Visual_Symptom_KitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
