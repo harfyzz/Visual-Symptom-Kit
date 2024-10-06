c97a12c9f93f2c4d91baa34b3de98bd5e9cd5569
Afeez <harfyzz@gmail.com>
2024-10-06 14:51:19 +0100

    -file set up

--- Visual Symptom Kit/ContentView.swift
+++ Visual Symptom Kit/ContentView.swift
@@ -6,14 +6,136 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct ContentView: View {
+    
+    @State private var isPresented: Bool = false
+    @State private var isBack: Bool = false
+    @StateObject private var bodyView = BodyViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("How do you feel?")
+                .font(.headline)
+                .foregroundStyle(Color("text.primary"))
+            Spacer()
+            ZStack {
+                VStack {
+                    HStack {
+                        VStack{
+                            Image(systemName: "waveform.path.ecg")
+                                .padding(4)
+                                .foregroundStyle(.white)
+                                .background(Color.green)
+                                .clipShape(Circle())
+                            
+                            Text("120/80")
+                                .font(.largeTitle)
+                                .fontWeight(.medium)
+                        }
+                        Spacer()
+                        VStack{
+                            Image(systemName: "heart.fill")
+                                .padding(4)
+                                .foregroundStyle(.white)
+                                .background(Color.red)
+                                .clipShape(Circle())
+                            
+                            HStack (alignment: .bottom) {
+                                Text("82")
+                                    .font(.largeTitle)
+                                    .fontWeight(.medium)
+                                Text("BPM")
+                                    .fontWeight(.medium)
+                                    .foregroundStyle(.secondary)
+                                    .frame(height: 30)
+                            }
+                        }
+                    }
+                    Spacer()
+                }
+                .padding(.top, 32)
+                bodyView.view()
+            }
+            VStack (spacing:16){
+                
+                    ZStack {
+                        
+                        HStack{
+                            if isBack {
+                                Spacer()
+                                    .frame(width: 140, height: 50)
+                            }
+                            RoundedRectangle(cornerRadius: 64)
+                                .fill(.white)
+                                .frame(width: 140, height: 50)
+                                .shadow(
+                                    color: .black.opacity(0.02),
+                                    radius: 10,
+                                    x: 0,
+                                    y: 1
+                                )
+                            
+                            if !isBack {
+                                Spacer()
+                                    .frame(width: 140, height: 50)
+                            }
+                        }
+                        .padding(4)
+                        HStack{
+                            Button {
+                                withAnimation{
+                                    isBack = false
+                                }
+                            } label: {
+                                Text("Front")
+                                    .foregroundStyle(
+                                        Color(
+                                            isBack ? "text.secondary" :"text.primary"
+                                        )
+                                    )
+                                    .fontWeight(isBack ? .regular : .medium)
+                                    .frame(width: 140, height: 50)
+                                    
+                                
+                            }
+                            
+                            Button {
+                                withAnimation{
+                                    isBack = true
+                                }
+                            } label: {
+                                Text("Back")
+                                    .foregroundStyle(
+                                        Color(isBack ? "text.primary" :"text.secondary")
+                                    )
+                                    .fontWeight(isBack ? .medium : .regular)
+                                    .frame(width: 140, height: 50)
+                                    
+                            }
+                        }
+                }
+                    .onChange(of: isBack) { oldValue, newValue in
+                        bodyView.setInput("back?", value: isBack)
+                    }
+                .background(
+                    RoundedRectangle(cornerRadius: 64)
+                        .fill(Color("bg.secondary"))
+                )
+                VStack{
+                    Text("Where do you feel pain or discomfort?")
+                        .font(.headline)
+                        .foregroundStyle(Color("text.primary"))
+                    
+                    Text(
+                        "Tap any body part in the image above that represents where you feel pain or discomfort."
+                    )
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                    .multilineTextAlignment(.center)
+                }
+            }
         }
         .padding()
     }
---
+++ Visual Symptom Kit/RiveHandler.swift
@@ -0,0 +1,43 @@
+//
+//  RiveHandler.swift
+//  Visual Symptom Kit
+//
+//  Created by Afeez Yunus on 06/10/2024.
+//
+
+import SwiftUI
+import RiveRuntime
+
+class BodyViewModel: RiveViewModel {
+    @Published var zoomState = ""
+    @Published var musclePart = ""// This will update your text based on the event value
+
+    init() {
+        super.init(fileName: "muscle_division", stateMachineName: "State Machine 1")
+    }
+    
+    func view() -> some View {
+        super.view().frame(width: 400, height: 400, alignment: .center)
+    }
+    // Subscribe to Rive events
+    @objc func onRiveEventReceived(onRiveEvent riveEvent: RiveEvent) {
+        if let generalEvent = riveEvent as? RiveGeneralEvent {
+            let eventProperties = generalEvent.properties()
+            
+            // Assuming the event has a property like "roomNumber"
+            if let zoomLevel = eventProperties["CTRL"] as? String {
+                zoomState = "\(zoomLevel)"  // Update the room number from 1 to 7
+            }
+            if let bodyPart = eventProperties["Body part"] as? String {
+                musclePart  = "\(bodyPart)"  // Update the room number from 1 to 7
+            }
+            
+        }
+        
+        debugPrint("Event Name: \(riveEvent.name())")
+        debugPrint("Event Type: \(riveEvent.type())")
+    }
+    
+
+    
+}
